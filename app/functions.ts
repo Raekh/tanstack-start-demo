e4d1d3ac56c8f85a3b555d30e166f4ac97de7e79
Raekh <encryptedhex@gmail.com>
2025-06-09 22:54:33 +0200

    server function validation

--- app/functions.ts
+++ app/functions.ts
@@ -1,5 +1,6 @@
 import { createServerFn } from "@tanstack/react-start";
 import * as fs from "node:fs";
+import * as v from "valibot";
 
 const filePath = "count.txt";
 
@@ -9,12 +10,20 @@ async function readCount() {
   );
 }
 
+const valibotSchema = v.union([
+  v.pipe(
+    v.string(),
+    v.transform((d) => parseInt(d)),
+  ),
+  v.number(),
+]);
+
 export const getCount = createServerFn({ method: "GET" }).handler(() => {
   return readCount();
 });
 
 export const updateCount = createServerFn({ method: "POST" })
-  .validator((d: number) => d)
+  .validator(valibotSchema)
   .handler(async ({ data }) => {
     const count = await readCount();
     await fs.promises.writeFile(filePath, (count + data).toString());
